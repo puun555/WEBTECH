5f209005502526080121cf34b8a1d2ebbcffd9de
puun555 <105053170+puun555@users.noreply.github.com>
2022-08-28 16:40:27 +0700

    up

--- lab3/2/scripts.js
+++ lab3/2/scripts.js
@@ -1,18 +1,22 @@
 function Check() {
     let pnum = document.getElementById('pNum').value;
+    let prefig = document.getElementById('prefig').value;
+    let fname = document.getElementById('firstName').value;
+    let lname = document.getElementById('lastName').value;
+    let add = document.getElementById('address').value;
+    let tbon = document.getElementById('tumbon').value;
+    let region = document.getElementById('region').value;
+    let city = document.getElementById('city').value;
+    let postnum = document.getElementById('postPass').value;
     // alert(pnum)
     if (pnum.toString().length != 13) {
         alert("หมายเลขบัตรประชาชนของคุณไม่ถูกต้อง")
         return false
     }
-    let prefig = document.getElementById('prefig').value;
     if (prefig == "") {
         alert("โปรดใส่คำนำหน้า")
         return false
     }
-
-    let fname = document.getElementById('firstName').value;
-    let lname = document.getElementById('lastName').value;
     if (fname.length < 2 || fname.length > 20) {
         alert("Name Incorrect")
         return false
@@ -21,10 +25,6 @@ function Check() {
         alert("Surname Incorrect")
         return  false
     }
-
-    let add = document.getElementById('address').value;
-    let tbon = document.getElementById('tumbon').value;
-    let region = document.getElementById('region').value;
     if (add.length < 5) {
         alert("Address Incorrect")
         return false
@@ -37,17 +37,12 @@ function Check() {
         alert("Region Inccorect")
         return false
     }
-    let city = document.getElementById('city').value;
     if (city == "") {
         alert('โปรดเลือกจังหวัด')
         return false;
     }
-
-    let postnum = document.getElementById('postPass').value;
     if (postnum.toString().length != 5) {
         alert("Post Number Incorrect")
         return false
     }
-
-    
 }
